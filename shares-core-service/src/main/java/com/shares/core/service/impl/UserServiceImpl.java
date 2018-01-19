94329310d45ae955222bf5ba339575f57a12e57c
568890375@qq.com <568890375gsd>
2018-01-19 18:06:55 +0800

    代码优化

--- shares-core-service/src/main/java/com/shares/core/service/impl/UserServiceImpl.java
+++ shares-core-service/src/main/java/com/shares/core/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.shares.core.service.impl;
 
 import com.shares.common.dal.daointerface.UserMapper;
 import com.shares.common.dal.daoobject.UserDO;
+import com.shares.common.dal.daoobject.UserParamDO;
+import com.shares.common.dal.plugin.common.model.PageRequest;
 import com.shares.core.model.bo.*;
 import com.shares.core.service.UserService;
 import com.shares.core.service.base.BeanServiceUtil;
@@ -29,8 +31,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<UserBO> findAll() {
-        List<UserDO> userDOS = userMapper.findAll();
+    public List<UserBO> listUser(PageRequest<UserParamBO> pageRequest) {
+        PageRequest<UserParamDO> request = new PageRequest<>();
+        BeanUtils.copyProperties(pageRequest, request);
+        UserParamDO paramDO = new UserParamDO();
+        if (pageRequest.getParam() != null) {
+            BeanUtils.copyProperties(pageRequest.getParam(), paramDO);
+        }
+        List<UserDO> userDOS = userMapper.listUserByPage(request);
         return BeanServiceUtil.copy(userDOS, UserBO.class);
     }
 
@@ -58,13 +66,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserBO login(String username, String password) {
         UserDO userDO = userMapper.login(username, password);
+        if (userDO == null) {
+            return null;
+        }
         UserBO userBO = new UserBO();
         BeanUtils.copyProperties(userDO, userBO);
         return userBO;
-    }
-
-    @Override
-    public void updateLastLoginTime(UserBO user) {
-
     }
 }
